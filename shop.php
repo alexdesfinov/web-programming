39795a6f45571bcdb72ccf0476a0041390e03da1
Alex Desfinov <alexdesfinov@gmail.com>
2024-01-12 15:11:35 +0700

    REvisi 3

--- shop.php
+++ shop.php
@@ -53,7 +53,7 @@ include("functions/myFunction.php");
                                         <p class="card-text"><?php echo $row['product_description']; ?></p>
                                         <div class="card-footer d-flex align-items-end mt-auto bg-white">
                                             <a href="includes/controller/add_to_cart.php?product_id=<?php echo $row['product_id']; ?>" class="btn cartcolor me-3"><i class="fa-solid fa-cart-shopping text-white"></i></a>
-                                            <a href="#!" class="btn cartcolor "><i class="fas fa-heart fa-lg text-white"></i></a>
+                                            <a href="includes/controller/add_to_wishlist.php?product_id=<?php echo $row['product_id']; ?>" class="btn cartcolor "><i class="fas fa-heart fa-lg text-white"></i></a>
                                         </div>
                                     </div>
                                 </div>
