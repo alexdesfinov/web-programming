98271c0f16d5d621b81467a1fbab89014a985cc6
Alex Desfinov <alexdesfinov@gmail.com>
2024-01-19 16:21:03 +0700

    REvisi lagi lagi

--- delete_from_cart.php
+++ delete_from_cart.php
@@ -5,7 +5,7 @@ include "functions/myFunction.php";
 $product_id = $_GET['product_id'];
 $user_id = $_SESSION['user_id'];
 
-$query = "DELETE FROM `mangola`.`cart` WHERE `product_id` LIKE '$product_id' AND `user_id` LIKE '$user_id'";
+$query = "DELETE FROM `toko_kue`.`cart` WHERE `product_id` LIKE '$product_id' AND `user_id` LIKE '$user_id'";
 if (mysqli_query($conn, $query)) {
 	header('Location: show_cart_items.php?msg=1');
 } else {
--- product_description.php
+++ product_description.php
@@ -1,16 +1,19 @@
-<html>
 <?php
 session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<?php
 
 include("functions/myFunction.php");
-
 
 include "includes/header.php";
 ?>
 
 <body style="background-image: url('images/bakery.jpeg'); background-size: cover; background-position: center;">
 	<?php
-	if (isset($_SESSION["login"])) {
+	if (isset($_SESSION['login'])) {
 		include "includes/header_postlogin.php";
 	} else {
 		include "includes/header_prelogin.php";
